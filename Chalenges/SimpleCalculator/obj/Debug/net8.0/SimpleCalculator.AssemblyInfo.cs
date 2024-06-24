7e9dfc957a5a13c61887aa306f7ad90a37b9c7d3
AmazingCodeLines <amazingcodelines@gmail.com>
2024-06-24 17:02:06 +0200

    Add gitignore file

--- Chalenges/SimpleCalculator/obj/Debug/net8.0/SimpleCalculator.AssemblyInfo.cs
+++ Chalenges/SimpleCalculator/obj/Debug/net8.0/SimpleCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a61b43e0c21a82c46100afe8e551e9b8646ee02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc6b98f0870cf22a241c0d89d4e8339897c524e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
